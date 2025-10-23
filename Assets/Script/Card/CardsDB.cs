2cb9eb1a551f5131ebefbcc171b11a3293456a81
Côme <come.santin@gmail.com>
2025-10-23 14:34:23 +0200

    [ADD] 2 card of each + [RANGEMENT]

--- Assets/Script/Card/CardsDB.cs
+++ Assets/Script/Card/CardsDB.cs
@@ -25,23 +25,25 @@ namespace CardRH
         private CardType _deckPhase = CardType.Skill;
         public void AddCard(CardView newCard)
         {
+            foreach (CardView card in _cardDeck) { if (newCard.cardData.Description == card.cardData.Description) return; }
+            int temp1 = 0;
+            int temp2 = 0;
             switch (newCard.cardData.Type)
             {
                 case CardType.Skill:
-                    _cardDeck[0].cardData = newCard.cardData.CreateClone();
-                    _cardDeck[0].cardData.InDeck = true;
-                    _cardDeck[0].gameObject.SetActive(true);
+                    temp1 = 0;
+                    temp2 = 1;
                     break;
                 
                 case CardType.SoftSkill:
-                    _cardDeck[1].cardData = newCard.cardData.CreateClone();
-                    _cardDeck[1].cardData.InDeck = true;
-                    _cardDeck[1].gameObject.SetActive(true);
+                    temp1 = 2;
+                    temp2 = 3;
                     break;
                 
                 case CardType.Context:
-                    foreach (CardView card in _cardDeck) { if (newCard.cardData.Description == card.cardData.Description) return; }
-                    if (!_cardDeck[2].gameObject.activeSelf)
+                    temp1 = 4;
+                    temp2 = 5;
+                    /*if (!_cardDeck[2].gameObject.activeSelf)
                     {
                         _cardDeck[2].cardData = newCard.cardData.CreateClone();
                         _cardDeck[2].cardData.InDeck = true;
@@ -64,13 +66,26 @@ namespace CardRH
                         _cardDeck[4].cardData = newCard.cardData.CreateClone();
                         _cardDeck[4].cardData.InDeck = true;
                         _cardDeck[4].gameObject.SetActive(true);
-                    }
+                    }*/
                     break;
                 
                 default:
                     break;
             }
 
+
+            if (!_cardDeck[temp1].gameObject.activeSelf)
+            {
+                _cardDeck[temp1].cardData = newCard.cardData.CreateClone();
+                _cardDeck[temp1].cardData.InDeck = true;
+                _cardDeck[temp1].gameObject.SetActive(true);
+            }
+            else /*if(!_cardDeck[temp2].gameObject.activeSelf)*/
+            {
+                _cardDeck[temp2].cardData = newCard.cardData.CreateClone();
+                _cardDeck[temp2].cardData.InDeck = true;
+                _cardDeck[temp2].gameObject.SetActive(true);
+            }
             DisplayDeck();
         }
 
@@ -138,44 +153,43 @@ namespace CardRH
             {
                 case CardType.Empty :
                     _listSkill.SetActive(true);
-                    GameObject firstParent = _cardDeck[0].transform.parent.gameObject;
-                    firstParent.SetActive(true);
+                    _cardDeck[0].transform.parent.gameObject.SetActive(true);
+                    _cardDeck[1].transform.parent.gameObject.SetActive(true);
                     _deckPhase = CardType.Skill;
                     _textMesh.text = "Choisie le savoir faire que tu recherhce";
                     break;
                 
                 case CardType.Skill :
-                    if (_cardDeck[0].cardData.Type == CardType.Empty) return;
+                    if (_cardDeck[0].cardData.Type == CardType.Empty 
+                        || _cardDeck[1].cardData.Type == CardType.Empty) return;
                     _listSkill.SetActive(false);
                     _listSoftSkill.SetActive(true);
-                    GameObject skillParent = _cardDeck[0].transform.parent.gameObject;
-                    skillParent.SetActive(false);
-                    GameObject softParent = _cardDeck[1].transform.parent.gameObject;
-                    softParent.SetActive(true);
+                    _cardDeck[0].transform.parent.gameObject.SetActive(false);
+                    _cardDeck[1].transform.parent.gameObject.SetActive(false);
+                    
+                    _cardDeck[2].transform.parent.gameObject.SetActive(true);
+                    _cardDeck[3].transform.parent.gameObject.SetActive(true);
                     _textMesh.text = "Choisie le savoir être que tu recherhce";
                     _deckPhase = CardType.SoftSkill;
                     break;
             
                 case CardType.SoftSkill :
-                    if (_cardDeck[1].cardData.Type == CardType.Empty) return;
+                    if (_cardDeck[2].cardData.Type == CardType.Empty 
+                        || _cardDeck[3].cardData.Type == CardType.Empty) return;
                     _listSoftSkill.SetActive(false);
                     _listContext.SetActive(true);
-                    GameObject softParent1 = _cardDeck[1].transform.parent.gameObject;
-                    softParent1.SetActive(false);
-                    GameObject contextParent = _cardDeck[2].transform.parent.gameObject;
-                    contextParent.SetActive(true);
-                    GameObject contextParent1 = _cardDeck[3].transform.parent.gameObject;
-                    contextParent1.SetActive(true);
-                    GameObject contextParent2 = _cardDeck[4].transform.parent.gameObject;
-                    contextParent2.SetActive(true);
+                    _cardDeck[2].transform.parent.gameObject.SetActive(false);
+                    _cardDeck[3].transform.parent.gameObject.SetActive(false);
+                    
+                    _cardDeck[4].transform.parent.gameObject.SetActive(true);
+                    _cardDeck[5].transform.parent.gameObject.SetActive(true);
                     _textMesh.text = "Choisie les contexte de job que tu recherhce";
                     _deckPhase = CardType.Context;
                     break;
             
                 case CardType.Context :
-                    if (_cardDeck[2].cardData.Type == CardType.Empty
-                        || _cardDeck[3].cardData.Type == CardType.Empty
-                        || _cardDeck[4].cardData.Type == CardType.Empty) return;
+                    if (_cardDeck[4].cardData.Type == CardType.Empty
+                        || _cardDeck[5].cardData.Type == CardType.Empty) return;
                     _textMesh.text = "Voici ton deck, est ce que il te convient?";
                     _listContext.SetActive(false);
                     DisplayFinalDeck();
