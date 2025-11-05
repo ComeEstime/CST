09b10107abb27906cc01139e36bbd58baae4bc80
Côme <come.santin@gmail.com>
2025-11-05 14:33:01 +0100

    [ADD] PlaceBtn + police update

--- Assets/Script/Canva/MainHUD.cs
+++ Assets/Script/Canva/MainHUD.cs
@@ -8,10 +8,30 @@ public class MainHUD : MonoBehaviour
 
     public void SetCard(List<CardSO> cardInfo)
     {
-        for (int i = 0; i < _cards.Count; i++)
-        {
-            _cards[i].SetData(cardInfo[i]);
-            _cards[i].UpdateCardUI();
-        }
+        if (cardInfo.Count < 6) return;
+        
+        //1
+        _cards[0].SetData(cardInfo[0]);
+        _cards[0].UpdateCardUI();
+        
+        //2
+        _cards[1].SetData(cardInfo[2]);
+        _cards[1].UpdateCardUI();
+        
+        //3
+        _cards[2].SetData(cardInfo[4]);
+        _cards[2].UpdateCardUI();
+        
+        //4
+        _cards[3].SetData(cardInfo[1]);
+        _cards[3].UpdateCardUI();
+        
+        //5
+        _cards[4].SetData(cardInfo[3]);
+        _cards[4].UpdateCardUI();
+        
+        //6
+        _cards[5].SetData(cardInfo[5]);
+        _cards[5].UpdateCardUI();
     }
 }
